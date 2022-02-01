0bf98abc9aa9bcd2d5cff0579d14e37dbb2791a7
aprillove <aprilclove86@gmail.com>
2022-02-01 16:50:29 -0500

    all files correct, still gotta style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Header(props) {
     return (
         <nav className='header'>
-            <Link to='/'>
+            <Link to='/about'>
                 <div className='title'>
                     $(fetch) Rewards You Can Afford
                     </div>
